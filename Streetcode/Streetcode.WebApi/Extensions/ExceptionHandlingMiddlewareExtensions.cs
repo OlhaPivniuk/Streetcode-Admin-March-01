25c2bf05a98aa1cd43d5856fb4f8b2a93b2152b8
Ianina Voda <yaninavoda@gmail.com>
2024-03-28 22:39:50 +0200

    add ExceptionHandlingMiddlewareExtensions

---
+++ Streetcode/Streetcode.WebApi/Extensions/ExceptionHandlingMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Streetcode.WebApi.Middleware.ExceptionHandlingMiddleware;
+
+namespace Streetcode.WebApi.Extensions;
+
+public static class ExceptionHandlingMiddlewareExtensions
+{
+    public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
+
+    public static void AddExceptionHandlingMiddlewareToServices(this IServiceCollection services)
+    {
+        services.AddTransient<ExceptionHandlingMiddleware>();
+    }
+}
