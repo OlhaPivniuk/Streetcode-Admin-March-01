3edb4cab2dd28f74dbc239ce3fd4cec31a390435
Ianina Voda <yaninavoda@gmail.com>
2024-03-28 22:27:55 +0200

    add ExceptionHandlingMiddleware

---
+++ Streetcode/Streetcode.WebApi/Middleware/ExceptionHandlingMiddleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,88 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Streetcode.WebApi.Middleware.ExceptionHandlingMiddleware;
+
+public class ExceptionHandlingMiddleware : IMiddleware
+{
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+    {
+        var traceId = Guid.NewGuid();
+        LogInfoAboutError(traceId, ex.Message, ex.StackTrace);
+
+        var problemDetails = new ProblemDetails();
+
+        switch (ex)
+        {
+            case ValidationException validationException:
+                {
+                    WriteInfoAboutErrorToProblemDetails(
+                        problemDetails,
+                        "Validation error",
+                        StatusCodes.Status400BadRequest,
+                        context.Request.Path,
+                        $"One or more validation errors occured, traceID: {traceId}");
+
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+                    if (validationException.Errors is not null)
+                    {
+                        problemDetails.Extensions["errors"] = validationException.Errors;
+                    }
+
+                    break;
+                }
+
+            default:
+                {
+                    WriteInfoAboutErrorToProblemDetails(
+                        problemDetails,
+                        "Internal Server Error",
+                        StatusCodes.Status500InternalServerError,
+                        context.Request.Path,
+                        $"Internal server error occured, traceID: {traceId}");
+
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                    break;
+                }
+        }
+    }
+
+    private static void WriteInfoAboutErrorToProblemDetails(
+        ProblemDetails problemDetails,
+        string errorTitle,
+        int errorStatusCode,
+        string errorInstance,
+        string errorDetail)
+    {
+        problemDetails.Title = errorTitle;
+        problemDetails.Status = errorStatusCode;
+        problemDetails.Instance = errorInstance;
+        problemDetails.Detail = errorDetail;
+    }
+
+    private void LogInfoAboutError(Guid traceId, string message, string? stackTrace)
+    {
+        _logger.LogError($"Error occure while processing the request, TraceID: {traceId}, Message: {message}, StackTrace: {stackTrace}");
+    }
+}
