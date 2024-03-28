98978735684d5bcdf6383b8400e1ebed60c5bf9f
Ianina Voda <yaninavoda@gmail.com>
2024-03-28 21:18:59 +0200

    add CreateFactCommandValidator and package FluentValidation

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Fact/Create/CreateFactCommandValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Fact.Create;
+
+internal class CreateFactCommandValidator : AbstractValidator<CreateFactCommand>
+{
+    private readonly int _maxTitleLength;
+    private readonly int _maxFactContentLength;
+
+    public CreateFactCommandValidator()
+    {
+        _maxTitleLength = 68;
+        _maxFactContentLength = 600;
+
+        RuleFor(command => command.Request.Title)
+            .NotEmpty()
+            .WithMessage("Title is required.")
+            .MaximumLength(_maxTitleLength)
+            .WithMessage($"Title length should not be longer than {_maxTitleLength} symbols.");
+
+        RuleFor(command => command.Request.FactContent)
+            .NotEmpty()
+            .WithMessage("Fact content is required.")
+            .MaximumLength(_maxFactContentLength)
+            .WithMessage($"Fact content length should not be longer than {_maxTitleLength} symbols.");
+
+        RuleFor(command => command.Request.ImageId)
+            .GreaterThan(0)
+            .WithMessage("Image id should be greater than 0.");
+
+        RuleFor(command => command.Request.StreetcodeId)
+            .GreaterThan(0)
+            .WithMessage("Streetcode id should be greater than 0.");
+    }
+}
