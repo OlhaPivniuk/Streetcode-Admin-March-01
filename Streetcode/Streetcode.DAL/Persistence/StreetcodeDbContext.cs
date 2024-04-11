3486150cf95510e0f960d30614a668d743725a9a
Ianina Voda <yaninavoda@gmail.com>
2024-04-11 21:13:17 +0300

    fix sonar warning

--- Streetcode/Streetcode.DAL/Persistence/StreetcodeDbContext.cs
+++ Streetcode/Streetcode.DAL/Persistence/StreetcodeDbContext.cs
@@ -72,12 +72,12 @@ public class StreetcodeDbContext : IdentityDbContext<ApplicationUser, Applicatio
     public DbSet<StreetcodePartner> StreetcodePartners { get; set; }
     public DbSet<TeamMemberPositions> TeamMemberPosition { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        base.OnModelCreating(modelBuilder);
+        base.OnModelCreating(builder);
 
-        modelBuilder.UseCollation("SQL_Ukrainian_CP1251_CI_AS");
+        builder.UseCollation("SQL_Ukrainian_CP1251_CI_AS");
 
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(NewsEntityTypeConfiguration).Assembly);
+        builder.ApplyConfigurationsFromAssembly(typeof(NewsEntityTypeConfiguration).Assembly);
     }
 }
